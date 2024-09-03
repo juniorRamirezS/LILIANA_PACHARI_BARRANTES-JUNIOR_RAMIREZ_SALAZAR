409c5271809d4d2ef6d7339c589e4082a1b4bfb9
juniorRamirezS <jvnioraldair93@gmail.com>
2024-09-02 23:37:34 -0500

    actualizando

--- Clinica/src/main/java/com/dh/Clinica/controller/TurnoController.java
+++ Clinica/src/main/java/com/dh/Clinica/controller/TurnoController.java
@@ -1,19 +1,23 @@
 package com.dh.Clinica.controller;
 
-import com.dh.Clinica.dto.request.TurnoModifyDto;
-import com.dh.Clinica.dto.request.TurnoRequestDto;
-import com.dh.Clinica.dto.response.TurnoResponseDto;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.dh.Clinica.dto.request.TurnoModifyDto;
 import com.dh.Clinica.dto.request.TurnoRequestDto;
 import com.dh.Clinica.dto.response.TurnoResponseDto;
 import com.dh.Clinica.entity.Turno;
 import com.dh.Clinica.service.ITurnoService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/turnos")
@@ -25,22 +29,13 @@ public class TurnoController {
     }
 
     @PostMapping("/guardar")
-    public ResponseEntity<TurnoResponseDto> guardarTurno(@RequestBody TurnoRequestDto turnoRequestDto) {
-        return ResponseEntity.ok(turnoService.guardarTurno(turnoRequestDto));
-    public ResponseEntity<TurnoResponseDto> guardarTurno(@RequestBody TurnoRequestDto turnoRequestDto){
+        public ResponseEntity<TurnoResponseDto> guardarTurno(@RequestBody TurnoRequestDto turnoRequestDto){
         return ResponseEntity.ok(turnoService.guardarTurno(turnoRequestDto));
     }
 
     @GetMapping("/buscartodos")
-    public ResponseEntity<List<TurnoResponseDto>> buscarTodos() {
     public ResponseEntity<List<TurnoResponseDto>> buscarTodos(){
         return ResponseEntity.ok(turnoService.buscarTodos());
-    }
-
-    @PutMapping("/modificar")
-    public ResponseEntity<String> modificarTurno(@RequestBody TurnoModifyDto turnoModifyDto) {
-        turnoService.modificarTurnos(turnoModifyDto);
-        return ResponseEntity.ok("{\"mensaje\": \"El turno fue modificado\"}");
     }
 
     @GetMapping("/buscarTurnoApellido/{apellido}")
